1fa0e8fa4246ac47c4da5839677f28f676960642
ivan4vl <60119228+ivan4vl@users.noreply.github.com>
2020-02-08 22:28:40 -0500

    Add files via upload

---
+++ Week3-2/server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname +'/views/partials');
+app.use(express.static(__dirname+'/public'));
+app.use(express.urlencoded({extend:false}));
+
+hbs.registerHelper('Table',(num,Hex)=>{
+    var str = '';
+   str += '<table style="width:35%;height:35%">';
+   str += '<tbody>';
+
+    
+    for(let i = 0; i < num; i++){
+        var color = ((1<<24)*Math.random()|0).toString(16);
+        var color1 = ((1<<24)*Math.random()|0).toString(16);
+        var color2 = ((1<<24)*Math.random()|0).toString(16);
+
+        Hex = color;
+        Hex1 = color1;
+        Hex2 = color2;
+
+        
+        str+='<tr><td style="background-color: #'+Hex+';">'+Hex+'<br/><span style="color:#ffffff">'+Hex.toUpperCase()+'</span></td> <td style="background-color: #'+Hex1+';">'+Hex1+'<br/><span style="color:#ffffff">'+Hex1.toUpperCase()+'</span></td> <td style="background-color: #'+Hex2+';">'+Hex2+'<br/><span style="color:#ffffff">'+Hex2.toUpperCase()+'</span></td></tr>';
+ 
+    }
+
+    str += '</tbody>';
+    str += '</table>';
+   
+    
+    return new hbs.handlebars.SafeString(str);
+})
+
+
+hbs.registerHelper('error404',(num,errorNumber)=>{
+    var str = '';   
+    for(i=0;i<num; i++){
+        str+='<div class="still">';
+        str+= errorNumber;
+        str+='</div>';
+        str+='<div class="rotate">';
+        str+= errorNumber;
+        str+='</div>';
+        str+='<div class="shrink">';
+        str+= errorNumber;
+        str+='</div>';
+    }
+    return new hbs.handlebars.SafeString(str);
+})
+
+function rando(req,res,next)
+{
+    req.num = Math.round(Math.random()*(50 - 20 + 1)) + 20;
+    next();
+}
+
+app.use(rando);
+
+
+app.post("/table", function(req,res){
+    res.render("index", {name:"Fidel's Lab 3", selection:req.body.selectNumber});
+});
+
+app.get('/', (req, res)=>{
+    res.render('index', {name:"Fidel's Lab 3"});
+});
+
+app.get('/404',(req,res)=>{
+    res.render('error',{number:req.num});
+});
+
+app.listen(3000,()=>{console.log('Server is running at localhost:3000')});
